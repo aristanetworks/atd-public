9b9368f59feaa62c266a2b6a7f5d1056597e3d48
Rob Martin <robmartin@arista.com>
2019-04-15 23:03:38 -0500

    Added logic to update arista labvm password in the labguides

--- labvm/services/atdFiles/atdFiles.sh
+++ labvm/services/atdFiles/atdFiles.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 # Find out what topology is running
-TOPO=`cat /etc/ACCESS_INFO.yaml | shyaml get-value topology`
+TOPO=$(cat /etc/ACCESS_INFO.yaml | shyaml get-value topology)
+ARISTA_PWD=$(cat /etc/ACCESS_INFO.yaml | shyaml get-value login_info.jump_host.pw)
 
 # Clean up previous stuff to make sure it's current
 rm -rf /var/www/html/atd/labguides/
 
 # Add files to arista home
 cp -R /tmp/atd/topologies/$TOPO/files/* /home/arista
+
+# Update the Arista user password for connecting to the labvm
+sed -i "s/{REPLACE_PWD}/$ARISTA_PWD/g" /tmp/atd/labguides/source/connecting.rst
 
 # Build the lab guides html files
 cd /tmp/atd/labguides
