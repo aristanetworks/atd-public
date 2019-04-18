e76700b5761878f982c3a51780724b1acc6604c5
Rob Martin <robmartin@arista.com>
2019-04-18 15:07:29 -0500

    Corrected version api call

--- labvm/services/cvpUpdater/cvpUpdater.py
+++ labvm/services/cvpUpdater/cvpUpdater.py
@@ -109,7 +109,7 @@ class CVPCON():
 
     def checkVersion(self):
         if self._checkSession():
-            return(self._sendRequest("GET",self.cvp_api['cvpservice/checkVersion']))
+            return(self._sendRequest("GET",self.cvp_api['checkVersion']))
     
     def saveTopology(self):
         """
