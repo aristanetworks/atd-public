2e5f150a43b1833d89300753eb3350b2e4554c9a
Rob Martin <robmartin@arista.com>
2019-06-14 14:29:30 -0500

    Updated filepath for serviceUpdater ref

--- labvm/services/atdPipUpdater/atdPipUpdater.sh
+++ labvm/services/atdPipUpdater/atdPipUpdater.sh
@@ -2,14 +2,14 @@
 
 # Get the current list of installable packages
 
-PIP_INST=$(cat serviceUpdater.yml | shyaml get-value pipPackages.Install)
-PIP_REM=$(cat serviceUpdater.yml | shyaml get-value pipPackages.Remove)
+PIP_INST=$(cat /tmp/atd/labvm/services/serviceUpdater.yml | shyaml get-value pipPackages.Install)
+PIP_REM=$(cat /tmp/atd/labvm/services/serviceUpdater.yml | shyaml get-value pipPackages.Remove)
 
 # Evaluate if there are items within the list
 if [ "$PIP_INST" != "None" ]
 then
     # Grab full list and install all packages
-    N_PIP_PKGS=$(cat serviceUpdater.yml | shyaml get-values pipPackages.Install)
+    N_PIP_PKGS=$(cat /tmp/atd/labvm/services/serviceUpdater.yml | shyaml get-values pipPackages.Install)
     echo "Packages to be Added: " $N_PIP_PKGS
     pip install $N_PIP_PKGS
 else
@@ -18,7 +18,7 @@ fi
 if [ "$PIP_REM" != "None" ]
 then
     # Grab full list and uninstall all listed packages
-    R_PIP_PKGS=$(cat serviceUpdater.yml | shyaml get-values pipPackages.Remove)
+    R_PIP_PKGS=$(cat /tmp/atd/labvm/services/serviceUpdater.yml | shyaml get-values pipPackages.Remove)
     echo "Packages to be Removed: $R_PIP_PKGS"
     pip uninstall -y $R_PIP_PKGS
 else
