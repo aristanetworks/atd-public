77b9df7d8c55a435447c89178214ff3407b21bee
Rob Martin <robmartin@arista.com>
2019-12-02 14:40:26 -0600

    moved grant changes to datacenter - latest

--- topologies/datacenter-grant/files/Broadcaster/mcast-receiver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-iperf -s -u -B 239.103.1.1 -i 1 &
-iperf -s -u -B 239.103.1.2 -i 1 &
--- topologies/datacenter-grant/files/Broadcaster/mcast-source.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#echo Starting source 239.103,1,1-3 for 1800 seconds
-
-for i in {1..3} ; do
-    IP='239.103.1'
-    IP=$IP.$i
-    iperf -c $IP -u -b 0.125m -T 10 -t 1800 -i 1 -&
-done
