17afec97f370333c5996ae06769c2ca8b39ba9a5
Rob Martin <robmartin@arista.com>
2020-01-29 08:58:49 -0600

    updated host config scripts for media to push mcast bash scripts

--- topologies/datacenter-latest/files/Broadcaster/pushHostDefaultConfig.sh
+++ topologies/datacenter-latest/files/Broadcaster/pushHostDefaultConfig.sh
@@ -2,18 +2,18 @@
 
 rm ~/enable-media
 
-echo "Prepping host vms and enabling eAPI"
-ssh -t 192.168.0.31 "
-enable
-configure
-aaa authorization exec default local"
+# echo "Prepping host vms and enabling eAPI"
+# ssh -t 192.168.0.31 "
+# enable
+# configure
+# aaa authorization exec default local"
 
-ssh -t 192.168.0.32 "
-enable
-configure
-aaa authorization exec default local"
+# ssh -t 192.168.0.32 "
+# enable
+# configure
+# aaa authorization exec default local"
 
-echo "Loading Configs"
-#config replace
-ssh -t 192.168.0.31 "configure replace flash:default-host1.cfg"
-ssh -t 192.168.0.32 "configure replace flash:default-host2.cfg"
+# echo "Loading Configs"
+# #config replace
+# ssh -t 192.168.0.31 "configure replace flash:default-host1.cfg"
+# ssh -t 192.168.0.32 "configure replace flash:default-host2.cfg"
