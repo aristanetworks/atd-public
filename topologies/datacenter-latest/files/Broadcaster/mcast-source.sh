01670d2351c9e377111e6f0190cf4ff397b41933
RyanM-Arista <44282350+RyanM-Arista@users.noreply.github.com>
2020-03-04 20:09:37 -0500

    update sender side script
    Updating source side script to prefer et1 for specified multicast streams instead of underlay interface

--- topologies/datacenter-latest/files/Broadcaster/mcast-source.sh
+++ topologies/datacenter-latest/files/Broadcaster/mcast-source.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-#echo Starting source 239.103,1,1-3 for 1800 seconds
+#echo Starting source 239.103.1.1-3 for 1800 seconds
+echo "Starting Sources"
+#adding routes in kernel to prefer et1 instead of ATD underlay interface
+sudo ip route add 239.103.1.1/32 dev et1
+sudo ip route add 239.103.1.2/32 dev et1
+sudo ip route add 239.103.1.3/32 dev et1
 
 for i in {1..3} ; do
     IP='239.103.1'
