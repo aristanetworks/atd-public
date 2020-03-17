75cad18968ddd91d34b79c8242df6fc13f190bed
Rob Martin <robmartin@arista.com>
2020-03-17 14:09:18 -0500

    modifying kvmbuild for atdadmin

--- nested-labvm/atd-docker/docker_build.sh
+++ nested-labvm/atd-docker/docker_build.sh
@@ -3,7 +3,7 @@
 cd /home/arista/atd-docker
 
 docker build --build-arg UID=$(id -u arista) -t atddocker_login:1.0 login/.
-docker build --build-arg UID=$(id -u arista) -t atddocker_kvmbuilder:1.0 kvmbuilder/.
+docker build --build-arg UID=$(id -u atdadmin) -t atddocker_kvmbuilder:1.0 kvmbuilder/.
 docker build --build-arg UID=$(id -u arista) -t atddocker_desktop:1.0 desktop/.
 docker build --build-arg UID=$(id -u arista) -t atddocker_cvpupdater:1.0 cvpUpdater/.
 docker build --build-arg UID=$(id -u arista) -t atddocker_gitconfigletsync:1.0 gitConfigletSync/.
