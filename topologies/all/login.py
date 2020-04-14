e59c295b3200201944e400d3bb75f38bd73b1d75
BG <bmgreer09@gmail.com>
2020-04-14 09:55:27 -0400

    updated login.py and atdFiles.sh to install pip3 and library for login.py to go to python3

--- topologies/all/login.py
+++ topologies/all/login.py
@@ -139,10 +139,6 @@ def lab_options_menu():
         print(menu_type.replace('-', ' ').replace('.yaml', '') + ":")
         for lab in menu_info['lab_list']:
           print("{0}. {1}".format(str(counter),menu_info['lab_list'][lab][0]['description']))
-          print(type(menu_info['lab_list'][lab][0]))
-          for item in menu_info['lab_list'][lab][0]:
-            print(type(item))
-            print(item['command'])
           commands_dict[counter] = menu_info['lab_list'][lab][0]['command']
           commands_dict[lab] = menu_info['lab_list'][lab][0]['command']
           counter += 1
