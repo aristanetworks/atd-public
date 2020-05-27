a610c27d1a14c4c60dc6aa0397b69f021fa8f08d
Rob Martin <robmartin@arista.com>
2020-05-27 10:17:40 -0500

    updated login logic

--- nested-labvm/atd-docker/login/login.py
+++ nested-labvm/atd-docker/login/login.py
@@ -54,8 +54,9 @@ def sort_veos(vd):
   tmp_d = {}
   fin_l = []
   for t_veos in vd:
-    tmp_l.append(t_veos['hostname'])
-    tmp_d[t_veos['hostname']] = t_veos
+    t_veos_name = list(t_veos.keys())[0]
+    tmp_l.append(t_veos_name)
+    tmp_d[t_veos_name] = t_veos
   tmp_l.sort(key=natural_keys)
   # If cvx in list, move to end
   if 'cvx' in tmp_l[0]:
