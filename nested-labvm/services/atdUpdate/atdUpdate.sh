1f203a3833cd8769758ade220669631bdbb707c6
Rob Martin <robmartin@arista.com>
2020-07-01 15:58:29 +0000

    updated scripts to modify gucamole user-mapping.xml

--- nested-labvm/services/atdUpdate/atdUpdate.sh
+++ nested-labvm/services/atdUpdate/atdUpdate.sh
@@ -2,6 +2,7 @@
 
 BRANCH=$(cat /etc/atd/ATD_REPO.yaml | python3 -m shyaml get-value atd-public-branch)
 TOPO=$(cat /etc/atd/ACCESS_INFO.yaml | python3 -m shyaml get-value topology)
+APWD=$(cat /etc/atd/ACCESS_INFO.yaml | python3 -m shyaml get-value login_info.jump_host.pw)
 
 rm -rf /opt/atd
 
@@ -11,6 +12,10 @@ git clone --branch $BRANCH https://github.com/aristanetworks/atd-public.git /opt
 ARISTA_SSH=$(cat /home/arista/.ssh/id_rsa.pub)
 
 sed -i "/username arista ssh-key/cusername arista ssh-key ${ARISTA_SSH}" /opt/atd/topologies/$TOPO/configlets/ATD-INFRA
+
+# Update arista user password for Guacamole
+
+sed -i "s/{ARISTA_REPLACE}/$APWD/g" /opt/atd/topologies/$TOPO/files/infra/user-mapping.xml
 
 # Update atdUpdate service
 
