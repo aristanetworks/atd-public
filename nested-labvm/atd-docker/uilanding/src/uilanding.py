e26d1c738d3d42cda0dd2d2c11ba9408b53ba63f
Rob Martin <robmartin@arista.com>
2020-07-02 16:09:54 +0000

    added auth to uilanding

--- nested-labvm/atd-docker/uilanding/src/uilanding.py
+++ nested-labvm/atd-docker/uilanding/src/uilanding.py
@@ -3,6 +3,8 @@
 import tornado.ioloop
 import tornado.web
 import requests
+import secrets
+import hashlib, uuid
 from datetime import datetime
 from ruamel.yaml import YAML
 
@@ -10,19 +12,65 @@ PORT = 80
 BASE_PATH = '/opt/topo/html/'
 ATD_ACCESS_PATH = '/etc/atd/ACCESS_INFO.yaml'
 
-class topoRequestHandler(tornado.web.RequestHandler):
+host_yaml = YAML().load(open(ATD_ACCESS_PATH, 'r'))
+
+salt = uuid.uuid4().hex
+
+accounts = {
+    hashlib.sha512((host_yaml['login_info']['jump_host']['user'] + salt).encode('utf-8')).hexdigest(): hashlib.sha512((host_yaml['login_info']['jump_host']['user'] + salt).encode('utf-8')).hexdigest()
+}
+
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return(self.get_secure_cookie("user"))
+
+class LoginHandler(BaseHandler):
     def get(self):
-        access_yaml = loadYAML()
-        node_ip = getPublicIP()
         self.render(
-            BASE_PATH + 'index.html',
-            ARISTA_PWD=access_yaml['login_info']['jump_host']['pw'],
-            PUBLIC_IP=node_ip
+            BASE_PATH + 'login.html',
+            LOGIN_MESSAGE=""
         )
+
+    def post(self):
+        tmp_username_hash = hashlib.sha512((self.get_argument("name") + salt).encode('utf-8')).hexdigest()
+        if tmp_username_hash in accounts:
+            tmp_pwd_hash = hashlib.sha512((self.get_argument("pwd") + salt).encode('utf-8')).hexdigest()
+            if tmp_pwd_hash == accounts[tmp_username_hash]:
+                self.set_secure_cookie("user", self.get_argument("name"))
+                self.redirect("/")
+            else:
+                self.render(
+                    BASE_PATH + 'login.html',
+                    LOGIN_MESSAGE="Wrong username and/or password."
+                )
+        else:
+            self.render(
+                BASE_PATH + 'login.html',
+                LOGIN_MESSAGE="Wrong username and/or password."
+            )
+
+class topoRequestHandler(BaseHandler):
+    def get(self):
+        if not self.current_user:
+            self.redirect('/login')
+            return()
+        else:
+            node_ip = getPublicIP()
+            self.render(
+                BASE_PATH + 'index.html',
+                ARISTA_PWD=host_yaml['login_info']['jump_host']['pw'],
+                PUBLIC_IP=node_ip
+            )
     
 # ===============================
 # Utility Functions
 # ===============================
+
+def genCookieSecret():
+    """
+    Function to generate a cookie_secret
+    """
+    return(secrets.token_hex(16))
 
 def getPublicIP():
     """
@@ -30,10 +78,6 @@ def getPublicIP():
     """
     response = requests.get('http://ipecho.net/plain')
     return(response.text)
-
-def loadYAML():
-    host_yaml = YAML().load(open(ATD_ACCESS_PATH, 'r'))
-    return(host_yaml)
 
 def pS(mtype):
     """
@@ -45,12 +89,17 @@ def pS(mtype):
 
 
 if __name__ == "__main__":
+    settings = {
+        'cookie_secret': genCookieSecret(),
+        'login_url': "/login"
+    }
     app = tornado.web.Application([
         (r'/js/(.*)', tornado.web.StaticFileHandler, {'path': BASE_PATH +  "js/"}),
         (r'/css/(.*)', tornado.web.StaticFileHandler, {'path': BASE_PATH +  "css/"}),
         (r'/images/(.*)', tornado.web.StaticFileHandler, {'path': BASE_PATH +  "images/"}),
-        (r'/', topoRequestHandler)
-    ])
+        (r'/', topoRequestHandler),
+        (r'/login', LoginHandler),
+    ], **settings)
     app.listen(PORT)
     print('*** Websocket Server Started on {} ***'.format(PORT))
     try:
