98e68ce7ac34862a21fbb86bc6e5d94f723cc2ee
Rob Martin <robmartin@arista.com>
2020-07-23 19:37:22 +0000

    removed ui reconfigure

--- topologies/all/labUI.py
+++ topologies/all/labUI.py
@@ -2,28 +2,17 @@
 
 from ruamel.yaml import YAML
 from bs4 import BeautifulSoup
-from os import system
 import tornado.ioloop
 import tornado.web
 import json
 import requests
 import traceback
-import syslog
 
 PORT = 50020
-pDEBUG = True
 BASE_PATH = '/home/arista/modules/'
-ACCESS_PATH = '/etc/ACCESS_INFO.yaml'
 MODULE_FILE = BASE_PATH + 'modules.yaml'
-LABS_FILE = BASE_PATH + 'labs.yaml'
-CONFIGURE_TOPOLOGY = "/usr/local/bin/ConfigureTopology.py"
-APP_KEY = 'app'
 
 MOD_YAML = YAML().load(open(MODULE_FILE, 'r'))
-
-ACCESS_YAML = YAML().load(open(ACCESS_PATH, 'r'))
-
-LABS_YAML = YAML().load(open(LABS_FILE, 'r'))
 
 settings = {
     'static_path': BASE_PATH
@@ -33,17 +22,6 @@ class topoRequestHandler(tornado.web.RequestHandler):
     def get(self):
         if 'lab' in self.request.arguments:
             lab_module = self.get_argument("lab")
-            if APP_KEY in ACCESS_YAML:
-                # If the app key/value differs, re-configure lab
-                if lab_module != ACCESS_YAML[APP_KEY]:
-                    if lab_module in LABS_YAML['labs']:
-                        lab_topo = LABS_YAML['labs'][lab_module]['topo']
-                        pS("INFO", "Re-configuring lab from {0} to {1}".format(ACCESS_YAML[APP_KEY], lab_module))
-                        system('echo -e "\n" | {0} -t {1} -l {2}'.format(CONFIGURE_TOPOLOGY, lab_topo, lab_module))
-                        pS("OK", "Done re-configuring the lab.")
-                        ACCESS_YAML[APP_KEY] =  lab_module
-                        with open(ACCESS_PATH, 'w') as mod_access:
-                            YAML().dump(ACCESS_YAML, mod_access)
             if lab_module in MOD_YAML:
                 labguide = getLabHTML(lab_module)
                 if labguide:
@@ -64,7 +42,7 @@ class topoRequestHandler(tornado.web.RequestHandler):
 # ===============================
 # Utility Functions
 # ===============================
-    
+
 def getPublicIP():
     """
     Function to get Public IP.
@@ -110,31 +88,17 @@ def parseLabHTML(html, lab_tag):
         if lab_tag in html_img.get("alt"):
             html_img.extract()
     return(parsed)
-
-def pS(mstat,mtype):
-    """
-    Function to send output from service file to Syslog
-    Parameters:
-    mstat = Message Status, ie "OK", "INFO" (required)
-    mtype = Message to be sent/displayed (required)
-    """
-    mmes = "\t" + mtype
-    syslog.syslog("[{0}] {1}".format(mstat,mmes.expandtabs(7 - len(mstat))))
-    if pDEBUG:
-        print("[{0}] {1}".format(mstat,mmes.expandtabs(7 - len(mstat))))
     
 
 if __name__ == "__main__":
-    # Open Syslog
-    syslog.openlog(logoption=syslog.LOG_PID)
     app = tornado.web.Application([
         (r'/module', topoRequestHandler),
         (r'/module/(.*)', tornado.web.StaticFileHandler, {'path': BASE_PATH })
     ],)
     app.listen(PORT)
-    pS("OK", '*** Websocket Server Started on {} ***'.format(PORT))
+    print('*** Websocket Server Started on {} ***'.format(PORT))
     try:
         tornado.ioloop.IOLoop.instance().start()
     except KeyboardInterrupt:
         tornado.ioloop.IOLoop.instance().stop()
-        pS("INFO", "*** Websocked Server Stopped ***")
+        print("*** Websocked Server Stopped ***")
