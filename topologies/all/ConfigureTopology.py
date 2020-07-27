04fc06cd1c1a2cf2bd6953e5ce63a8928e7f4551
Teren Sapp <teren@arista.com>
2020-07-27 18:37:45 +0000

    Check for CVP

--- topologies/all/ConfigureTopology.py
+++ topologies/all/ConfigureTopology.py
@@ -227,7 +227,7 @@ def main(argv):
         print("Setting up {0} lab").format(lab)
         for node in accessinfo["nodes"]["veos"]:
             hostname = node["hostname"]
-            baseConfigs = cvpInfo["configlets"]["netelements"]
+            baseConfigs = cvpInfo["cvpinfo"]["configlets"]["netelements"]
             print("Config for {0}: {1} {2}").format(hostname,baseConfigs[hostname],labconfiglets[lab][hostname])
 
 
