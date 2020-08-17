8a9303d59b9a26c6389fb808431d1cd61fef4d55
Rob Martin <39608748+networkRob@users.noreply.github.com>
2020-08-17 07:20:02 -0500

    Update atdFiles.sh
    upgrading pip3

--- labvm/services/atdFiles/atdFiles.sh
+++ labvm/services/atdFiles/atdFiles.sh
@@ -35,6 +35,7 @@ curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 apt install python3-pip nodejs -y
 
 # Install python3 ruamel.yaml
+pip3 install --upgrade pip
 pip3 install ruamel.yaml bs4 tornado scp paramiko rcvpapi
 
 # Setup NPM and webssh2
