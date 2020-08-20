b015e77495d378dfae5638a51d28a226505e44c5
ccnPeon <bmgreer09@gmail.com>
2020-08-20 16:43:33 -0400

    added __init__ and fixed space entry in user input

--- topologies/all/ConfigureTopology.py
+++ topologies/all/ConfigureTopology.py
@@ -30,7 +30,7 @@ zerotouch cancel
 """
 class ConfigureTopology():
 
-    def remove_configlets(self, client, device, lab_configlets):
+    def remove_configlets(client, device, lab_configlets):
         """
         Removes all configlets except the ones defined here or starting with SYS_
         Define base configlets that are to be untouched
@@ -56,7 +56,7 @@ class ConfigureTopology():
         client.applyConfiglets(device)
 
 
-    def get_device_info(self,client):
+    def get_device_info(client):
         eos_devices = []
         for dev in client.inventory:
             tmp_eos = client.inventory[dev]
@@ -66,7 +66,7 @@ class ConfigureTopology():
         return(eos_devices)
 
 
-    def update_topology(self,client,lab,configlets):
+    def update_topology(client,lab,configlets):
         # Get all the devices in CVP
         devices = self.get_device_info(client)
         # Loop through all devices
@@ -90,7 +90,7 @@ class ConfigureTopology():
         # Perform a single Save Topology by default
         client.saveTopology()
 
-    def print_usage(self,topologies):
+    def print_usage(topologies):
         # Function to print help menu with valid topologies
         print('Usage:')
         print('')
@@ -102,7 +102,7 @@ class ConfigureTopology():
         print('')
         quit()
 
-    def send_to_syslog(self,mstat,mtype):
+    def send_to_syslog(mstat,mtype):
         """
         Function to send output from service file to Syslog
         Parameters:
@@ -115,7 +115,7 @@ class ConfigureTopology():
             print("[{0}] {1}".format(mstat,mmes.expandtabs(7 - len(mstat))))
 
 
-    def push_bare_config(self,veos_host, veos_ip, veos_config):
+    def push_bare_config(veos_host, veos_ip, veos_config):
         """
         Pushes a bare config to the EOS device.
         """
@@ -143,7 +143,7 @@ class ConfigureTopology():
         veos_ssh.close()
         return(DEVREBOOT)
 
-    def deploy_lab(self,selected_menu,selected_lab):
+    def deploy_lab(selected_menu,selected_lab):
 
         # Check for additional commands in lab yaml file
         lab_file = open('/home/arista/menus/{0}'.format(selected_menu + '.yaml'))
