3a7dc58cf55efe1b489896c32ac9d80285054e01
ccnPeon <bmgreer09@gmail.com>
2020-08-20 17:05:54 -0400

    added __init__ and fixed space entry in user input

--- topologies/all/ConfigureTopology.py
+++ topologies/all/ConfigureTopology.py
@@ -144,7 +144,7 @@ class ConfigureTopology:
         veos_ssh.close()
         return(DEVREBOOT)
 
-    def deploy_lab(self,selected_menu,selected_lab):
+    def deploy_lab(selected_menu,selected_lab):
 
         # Check for additional commands in lab yaml file
         lab_file = open('/home/arista/menus/{0}'.format(selected_menu + '.yaml'))
