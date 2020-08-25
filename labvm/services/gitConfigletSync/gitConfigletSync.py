c4647224cef206e7788740a5cff368cfa3df086b
ccnPeon <bmgreer09@gmail.com>
2020-08-25 16:34:36 -0400

    bringing gitconfigletsync in line line master

--- labvm/services/gitConfigletSync/gitConfigletSync.py
+++ labvm/services/gitConfigletSync/gitConfigletSync.py
@@ -8,6 +8,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 pDEBUG = False
 topo_file = '/etc/ACCESS_INFO.yaml'
+CVP_CONFIG_FILE = '/home/arista/.cvpState.txt'
 
 def pS(mstat,mtype):
     """
@@ -60,18 +61,18 @@ def main():
    while not cvp_clnt:
       try:
          cvp_clnt = CVPCON(accessinfo['nodes']['cvp'][0]['internal_ip'],cvpUsername,cvpPassword)
-         print("[OK] Connected to CVP at {0}".format(accessinfo['nodes']['cvp'][0]['internal_ip']))
+         pS("OK", "Connected to CVP at {0}".format(accessinfo['nodes']['cvp'][0]['internal_ip']))
       except:
-         print("[ERROR] CVP is currently unavailable....Retrying in 30 seconds.")
+         pS("Error", "CVP is currently unavailable....Retrying in 30 seconds.")
          time.sleep(30)
 
    # Check if the repo has been downloaded
    while True:
       if os.path.isdir(gitTempPath):
-         print("Local copy exists....continuing")
+         pS("INFO", "Local copy exists....continuing")
          break
       else:
-         print("Local copy is missing....Waiting 1 minute for it to become available")
+         pS("ERROR", "Local copy is missing....Waiting 1 minute for it to become available")
          time.sleep(60)
 
    configlets = os.listdir(gitTempPath + configletPath)
@@ -84,12 +85,38 @@ def main():
          if '.py' in configletName:
             new_configletName = configletName.replace(".py","")
             res = cvp_clnt.impConfiglet("builder",new_configletName,configletConfig)
-            print("{0} Configlet Builder: {1}".format(res[0],new_configletName))
+            pS("INFO", "{0} Configlet Builder: {1}".format(res[0],new_configletName))
          else:
             res = cvp_clnt.impConfiglet("static",configletName,configletConfig)
-            print("{0} Configlet: {1}".format(res[0],configletName))
+            pS("INFO", "{0} Configlet: {1}".format(res[0],configletName))
+   # Perform a check to see if there any pending tasks to be executed due to configlet update
+   sleep(10)
+   pS("INFO", "Performing check for pending tasks.")
+   cvp_clnt.getAllTasks("pending")
+   if cvp_clnt.tasks['pending']:
+      pS("INFO", "Pending tasks found, will execute")
+      task_response = cvp_clnt.execAllTasks("pending")
+      # Perform check to see if there are any existing tasks to be executed
+      if task_response:
+         pS("INFO", "All pending tasks are executing")
+         for task_id in task_response['ids']:
+            task_status = cvp_clnt.getTaskStatus(task_id)['taskStatus']
+            while task_status != "Completed":
+               task_status = cvp_clnt.getTaskStatus(task_id)['taskStatus']
+               if task_status == 'Failed':
+                     pS("ERROR", "Task ID: {0} Status: {1}".format(task_id, task_status))
+                     break
+               elif task_status == 'Completed':
+                     pS("OK", "Task ID: {0} Status: {1}".format(task_id, task_status))
+                     break
+               else:
+                     pS("INFO", "Task ID: {0} Status: {1}, Waiting 10 seconds...".format(task_id, task_status))
+                     sleep(10)
+   else:
+      pS("OK", "No pending tasks found")
 
-   print("Configlet sync complete")
+
+   pS("OK", "Configlet sync complete")
 
 
 if __name__ == '__main__':
@@ -99,10 +126,14 @@ if __name__ == '__main__':
 
    atd_yaml = getTopoInfo(topo_file)
    if 'cvp' in atd_yaml['nodes']:
-      # Start the main Service
-      pS("OK","Initial ATD Topo Boot")
-      main()
-      pS("OK","Completed Configlet Sync")
+      # Check if cvpUpdater has run
+      if os.path.exists(CVP_CONFIG_FILE):
+         # Start the main Service
+         pS("INFO", "Starting configlet sync...")
+         main()
+         pS("OK","Completed Configlet Sync")
+      else:
+         pS("OK","Initial ATD Topo Boot, exiting")
    else:
       pS("INFO","CVP is not present in this topology, disabling gitConfigletSync")
       os.system("systemctl disable gitConfigletSync")
