6da406a5b166a2dbb08418b1df1a3af7f1d1b5d1
Rob Martin <robmartin@arista.com>
2020-09-14 20:09:46 +0000

    added missing mgmt veth creation

--- nested-labvm/atd-docker/ceosbuilder/ceosbuilder.py
+++ nested-labvm/atd-docker/ceosbuilder/ceosbuilder.py
@@ -186,6 +186,7 @@ def main(args):
         create_output.append("ip link set {0}-mgmt up\n".format(_node))
         create_output.append("sleep 1\n")
         create_output.append("docker run -d --name={0} --net=container:{0}-net --ip {1} --privileged -v {2}/{0}:/mnt/flash:Z -e INTFTYPE=et -e MGMT_INTF=eth0 -e ETBA=1 -e SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 -e CEOS=1 -e EOS_PLATFORM=ceoslab -e container=docker -i -t ceosimage:{3} /sbin/init systemd.setenv=INTFTYPE=et systemd.setenv=MGMT_INTF=eth0 systemd.setenv=ETBA=1 systemd.setenv=SKIP_ZEROTOUCH_BARRIER_IN_SYSDBINIT=1 systemd.setenv=CEOS=1 systemd.setenv=EOS_PLATFORM=ceoslab systemd.setenv=container=docker\n".format(_node, CEOS[_node].ip, CEOS_NODES, CEOS_VERSION))
+        startup_output.append("ip link add {0}-eth0 type veth peer name {0}-mgmt\n".format(_node))
         startup_output.append("brctl addif {0} {1}-mgmt\n".format(MGMT_BRIDGE, _node))
         startup_output.append("ip link set {0}-eth0 netns {0} name eth0 up\n".format(_node))
         startup_output.append("ip link set {0}-mgmt up\n".format(_node))
