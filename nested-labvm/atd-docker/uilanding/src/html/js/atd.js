1710665037bcbfbaab8d30c06541e8ed88aa2387
Rob Martin <robmartin@arista.com>
2020-10-29 15:54:56 +0000

    relocated javascript src load

--- nested-labvm/atd-docker/uilanding/src/html/js/atd.js
+++ nested-labvm/atd-docker/uilanding/src/html/js/atd.js
@@ -1,11 +1,3 @@
 var atdHostname = window.location.hostname;
 
-while(true) {
-    try {
-        document.getElementById("cvp_link").innerHTML = '<a href="https://' + atdHostname + '" target="_blank">CVP</a>';
-        break
-    } catch {
-        continue;
-    }
-}
-
+document.getElementById("cvp_link").innerHTML = '<a href="https://' + atdHostname + '" target="_blank">CVP</a>';
