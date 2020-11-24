711790129ffe19fdfdb6b04c13f953fd5a68f4d5
Rob Martin <robmartin@arista.com>
2020-11-24 15:42:05 +0000

    updated nginx ssl config

--- nested-labvm/atd-docker/nginx/src/atd-certs.sh
+++ nested-labvm/atd-docker/nginx/src/atd-certs.sh
@@ -2,11 +2,7 @@
 
 PROJECT=$(cut -d':' -f2 <<<$(grep project /etc/atd/ACCESS_INFO.yaml))
 
-if [ $PROJECT == 'atds-280712' ]
+if [ $PROJECT ]
 then
-  cp -r /opt/nginx/certs/prod/topo-atd.* /etc/nginx/certs
-  touch /etc/nginx/certs/prod.txt
-else
-  cp -r /opt/nginx/certs/dev/topo-atd.* /etc/nginx/certs
-  touch /etc/nginx/certs/dev.txt
+  cp -r /opt/nginx/certs/$PROJECT/* /etc/nginx/certs
 fi
