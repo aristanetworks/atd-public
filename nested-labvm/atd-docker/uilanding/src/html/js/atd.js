2ecf115f37e5a07ebc73d9026599def9f09edd32
Rob Martin <robmartin@arista.com>
2020-11-30 19:47:28 +0000

    updated uilanding

--- nested-labvm/atd-docker/uilanding/src/html/js/atd.js
+++
@@ -1,5 +0,0 @@
-var atdHostname = window.location.hostname;
-
-function updateCVP() {
-    document.getElementById("cvp_link").innerHTML = '<a href="https://' + atdHostname + '" target="_blank">CVP</a>';
-}
