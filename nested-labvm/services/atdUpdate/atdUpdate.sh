46614757ee8508ea7294b7b6705e20ac4811aa2f
Rob Martin <robmartin@arista.com>
2020-12-07 16:17:58 +0000

    adding last couple of updates

--- nested-labvm/services/atdUpdate/atdUpdate.sh
+++ nested-labvm/services/atdUpdate/atdUpdate.sh
@@ -24,7 +24,7 @@ git clone --branch $BRANCH $REPO /opt/atd
 
 # Update atdUpdate service
 
-rsync /opt/atd/nested-labvm/services/atdUpdate/atdUpdate.sh /usr/local/bin/
+rsync -av /opt/atd/nested-labvm/services/atdUpdate/atdUpdate.sh /usr/local/bin/
 
 # Update ssh-key in EOS configlet for Arista user
 ARISTA_SSH=$(cat /home/arista/.ssh/id_rsa.pub)
