511a1ef676353b3863f0002798ae2ca81e68a5d0
Rob Martin <robmartin@arista.com>
2021-02-03 20:41:52 +0000

    added catch for -core

--- nested-labvm/atd-docker/ceosbuilder/ceosbuilder.py
+++ nested-labvm/atd-docker/ceosbuilder/ceosbuilder.py
@@ -54,12 +54,22 @@ def parseNames(devName):
     numer = ''
     split_len = 2
     devDC = False
+    devCORE = False
     tmp_devName = ""
     if '-dc' in devName.lower() and 'dci' != devName.lower():
         _tmp = devName.split('-')
         tmp_devName = _tmp[0]
         if 'dc' in _tmp[1].lower():
             devDC = _tmp[1]
+        for char in tmp_devName:
+            if char.isalpha():
+                alpha += char
+            elif char.isdigit():
+                numer += char
+    elif '-core' in devName.lower():
+        _tmp = devName.split('-')
+        tmp_devName = _tmp[0]
+        devCORE = _tmp[1]
         for char in tmp_devName:
             if char.isalpha():
                 alpha += char
@@ -76,12 +86,14 @@ def parseNames(devName):
     else:
         dev_name = alpha[:split_len]
     if devDC:
-        dc_code = devDC.lower().replace('c','')
+        dev_code = devDC.lower().replace('c','')
+    elif devCORE:
+        dev_code = devCORE.lower().replace('ore','')
     else:
-        dc_code = ""
+        dev_code = ""
     devInfo = {
         'name': devName,
-        'code': dev_name + numer + dc_code,
+        'code': dev_name + numer + dev_code,
     }
     return(devInfo)
 
