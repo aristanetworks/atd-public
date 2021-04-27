f1e41bdf9b2b80e68cc6b74236b2d36959efd2d0
Rob Martin <39608748+networkRob@users.noreply.github.com>
2021-04-27 14:18:46 -0500

    Gcp docker (#292)
    * testing new  build
    
    * testing
    
    * updated gitconfigletsync
    
    * updated docker builds
    
    * updated compose
    
    * fixed image project
    
    * fixed tag typo
    
    * fixed tag typo
    
    * fixed tag typo
    
    * updated the cb files and updated kvmbuilder
    
    * updated compose with user for kvm
    
    * updated cvpupdater build
    
    * updated freerad container for volume mount
    
    * updated startup for ids and build
    
    * updated login for cloud build
    
    * bumped login and cvpupdater image versions in compose
    
    * disabled the execution of building local docker images
    
    * bumped freerad image in compose

--- nested-labvm/atd-docker/docker_build.sh
+++ nested-labvm/atd-docker/docker_build.sh
@@ -2,18 +2,21 @@
 
 # cd /home/arista/atd-docker
 
-docker build --build-arg UID=$(id -u arista) -t atddocker_login:1.1 login/.
-docker build --build-arg UID=$(id -u atdadmin) -t atddocker_kvmbuilder:1.1 kvmbuilder/.
-docker build --build-arg UID=$(id -u arista) -t atddocker_cvpupdater:1.0 cvpUpdater/.
-docker build --build-arg UID=$(id -u arista) -t atddocker_gitconfigletsync:1.0 gitConfigletSync/.
-# docker build --build-arg UID=$(id -u arista) -t atddocker_sslupdater:1.0 sslUpdater/.
-docker build -t atddocker_labguides:1.1 labguides/.
-docker build -t atddocker_freerad:1.0 freeradius/.
-docker build -t atddocker_nginx:1.1 nginx/.
-docker build -t atddocker_jenkins:1.0 jenkins/.
-docker build -t atddocker_uptime:1.0 uptime/.
-docker build -t atddocker_uilanding:1.0 uilanding/.
-docker build -t atddocker_ceosbuilder:1.0 ceosbuilder/.
-docker build -t atddocker_monitor:1.0 monitor/.
-docker build -t atddocker_coder:1.2 coder/.
-docker build --build-arg TAG=0.3.0 -t attdocker_ssh:1.0 ssh/.
+# docker build --build-arg UID=$(id -u arista) -t atddocker_login:1.1 login/.
+# docker build --build-arg UID=$(id -u atdadmin) -t atddocker_kvmbuilder:1.1 kvmbuilder/.
+# docker build --build-arg UID=$(id -u arista) -t atddocker_cvpupdater:1.0 cvpUpdater/.
+# docker build --build-arg UID=$(id -u arista) -t atddocker_gitconfigletsync:1.0 gitConfigletSync/.
+# docker build -t atddocker_labguides:1.1 labguides/.
+# docker build -t atddocker_freerad:1.0 freeradius/.
+# docker build -t atddocker_nginx:1.0 nginx/.
+# docker build -t atddocker_jenkins:1.0 jenkins/.
+# docker build -t atddocker_uptime:1.0 uptime/.
+# docker build -t atddocker_uilanding:1.0 uilanding/.
+# docker build -t atddocker_ceosbuilder:1.0 ceosbuilder/.
+# docker build -t atddocker_monitor:1.0 monitor/.
+# docker build -t atddocker_coder:1.2 coder/.
+# docker build --build-arg TAG=0.3.0 -t attdocker_ssh:1.0 ssh/.
+
+# Pull any remote repo images
+# docker pull mpergament/avd-cpv-atd-nested:latest
+# docker pull mpergament/aristarobot:latest
