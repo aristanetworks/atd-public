02388f6db12ee1c5a9261e41d97a89d140f10b78
Rob Martin <robmartin@arista.com>
2021-04-27 16:53:49 +0000

    updated freerad container for volume mount

--- nested-labvm/atd-docker/docker_build.sh
+++ nested-labvm/atd-docker/docker_build.sh
@@ -7,7 +7,7 @@ docker build --build-arg UID=$(id -u arista) -t atddocker_login:1.1 login/.
 # docker build --build-arg UID=$(id -u arista) -t atddocker_cvpupdater:1.0 cvpUpdater/.
 # docker build --build-arg UID=$(id -u arista) -t atddocker_gitconfigletsync:1.0 gitConfigletSync/.
 # docker build -t atddocker_labguides:1.1 labguides/.
-docker build -t atddocker_freerad:1.0 freeradius/.
+# docker build -t atddocker_freerad:1.0 freeradius/.
 # docker build -t atddocker_nginx:1.0 nginx/.
 # docker build -t atddocker_jenkins:1.0 jenkins/.
 # docker build -t atddocker_uptime:1.0 uptime/.
