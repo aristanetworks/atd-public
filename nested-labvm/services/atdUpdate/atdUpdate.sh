f622ba57ca6476ceb789372d261f93fae4e8512e
Rob Martin <39608748+networkRob@users.noreply.github.com>
2021-05-04 14:14:47 -0500

    Train docker 2020 2 (#311)
    * updated atdUpdate to perform checkout then pull
    
    * updated atdUpdate to update startup scripts only (#289)
    
    * Gcp docker (#292)
    
    * testing new  build
    
    * testing
    
    * updated gitconfigletsync
    
    * updated docker builds
    
    * updated compose
    
    * fixed image project
    
    * fixed tag typo
    
    * fixed tag typo
    
    * fixed tag typo
    
    * updated the cb files and updated kvmbuilder
    
    * updated compose with user for kvm
    
    * updated cvpupdater build
    
    * updated freerad container for volume mount
    
    * updated startup for ids and build
    
    * updated login for cloud build
    
    * bumped login and cvpupdater image versions in compose
    
    * disabled the execution of building local docker images
    
    * bumped freerad image in compose
    
    * force pdf labguides to generate (#296)
    
    * fixed volume permissions (#299)
    
    * modified docker-compose.yml under atd-coder, to add the /etc/ volume mount to be able to read /etc/atd/ACCESS_INFO.yaml for AVD-ATD setup (#302)
    
    * Avd atd 2021.1 ch (#305)
    
    * added shyaml to Dockerfile for atd-coder
    
    * changed atd-coder to image tag 2.0.1
    
    Co-authored-by: Corey Hines <chines@arista.com>

--- nested-labvm/services/atdUpdate/atdUpdate.sh
+++ nested-labvm/services/atdUpdate/atdUpdate.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
 BRANCH=$(cat /etc/atd/ATD_REPO.yaml | python3 -m shyaml get-value atd-public-branch)
-TOPO=$(cat /etc/atd/ACCESS_INFO.yaml | python3 -m shyaml get-value topology)
-APWD=$(cat /etc/atd/ACCESS_INFO.yaml | python3 -m shyaml get-value login_info.jump_host.pw)
 
 if  [ -z "$(cat /etc/atd/ATD_REPO.yaml | grep repo)" ]
 then
     REPO="https://github.com/aristanetworks/atd-public.git"
 else
     REPO=$(cat /etc/atd/ATD_REPO.yaml | python3 -m shyaml get-value public-repo)
-fi
-
-if [ "$(cat /etc/atd/ACCESS_INFO.yaml | grep eos_type)" ]
-then
-    EOS_TYPE=$(cat /etc/atd/ACCESS_INFO.yaml | python3 -m shyaml get-value eos_type)
-else
-    EOS_TYPE=veos
 fi
 
 # Perform git repo check
@@ -35,6 +26,7 @@ git fetch
 if [[ "$(git branch --show-current)" = "$BRANCH" ]]
 then
     echo "Target branch matches current branch"
+    git checkout .
     git pull
 else
     echo "Branches do not match, updating to branch $BRANCH"
@@ -43,75 +35,13 @@ else
     git pull
 fi
 
-# Update atdUpdate service
+# Update atdUpdate script
 
 rsync -av /opt/atd/nested-labvm/services/atdUpdate/atdUpdate.sh /usr/local/bin/
 
-# Update ssh-key in EOS configlet for Arista user
-ARISTA_SSH=$(cat /home/arista/.ssh/id_rsa.pub)
+# Update atdStartup script
 
-sed -i "/username arista ssh-key/cusername arista ssh-key ${ARISTA_SSH}" /opt/atd/topologies/$TOPO/configlets/ATD-INFRA
+rsync -av /opt/atd/nested-labvm/services/atdStartup/atdStartup.sh /usr/local/bin/
 
-# Update arista user password for Guacamole
-
-find /opt/atd/nested-labvm/atd-docker/*  -type f -print0 | xargs -0 sed -i "s/{ARISTA_REPLACE}/$APWD/g" 
-find /opt/atd/topologies/$TOPO/files/*  -type f -print0 | xargs -0 sed -i "s/{ARISTA_REPLACE}/$APWD/g" 
-
-
-
-# Update the base configlets for ceos/veos mgmt numbering
-
-if [ $EOS_TYPE == 'ceos' ]
-then
-    sed -i 's/Management1/Management0/g' /opt/atd/topologies/$TOPO/configlets/*
-fi
-
-# Copy topo image to app directory
-rsync -av /opt/atd/topologies/$TOPO/atd-topo.png /opt/atd/topologies/$TOPO/files/apps/uilanding
-
-# Add files to arista home
-rsync -av --update /opt/atd/topologies/$TOPO/files/ /home/arista/arista-dir
-rsync -av /opt/atd/topologies/$TOPO/files/infra /home/arista/
-
-# Perform check if there is a scripts directory
-if [ -d "/opt/atd/topologies/$TOPO/files/scripts" ]
-then
-    rsync -av /opt/atd/topologies/$TOPO/files/scripts /home/arista/GUI_Desktop/
-fi
-
-# Perform a check for the repo directory for datacenter
-if ! [ -d "/home/arista/arista-dir/apps/coder/labfiles/lab6/repo" ] && [ $TOPO == "datacenter" ]
-then
-    mkdir -p /home/arista/arista-dir/apps/coder/labfiles/lab6/repo
-    cd /home/arista/arista-dir/apps/coder/labfiles/lab6/repo
-    git init --bare
-fi
-
-chown -R arista:arista /home/arista
-
-# Update ATD containers
-
-cd /opt/atd/nested-labvm/atd-docker
-
-su atdadmin -c 'bash docker_build.sh'
-
-# Setting arista user ids for coder container
-export ArID=$(id -u arista)
-export ArGD=$(id -g arista)
-
-/usr/local/bin/docker-compose up -d --remove-orphans --force-recreate
-
-echo 'y' | docker image prune
-
-systemctl restart sshd
-
-# if cEOS Startup present, run it
-if [ -f "/opt/ceos/scripts/.ceos.txt" ]
-then
-    while : ; do
-        [[ -f "/opt/ceos/scripts/Startup.sh" ]] && break
-        echo "Pausing until file exists."
-        sleep 1
-    done
-    bash /opt/ceos/scripts/Startup.sh
-fi
+echo "Executing atdStartup"
+bash /usr/local/bin/atdStartup.sh
