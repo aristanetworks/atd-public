bb18bd19ddae6e8c254636cefe87383f32a3d1b7
Rob Martin <robmartin@arista.com>
2021-06-28 21:42:44 +0000

    adding some logic

--- nested-labvm/atd-docker/confTopo/src/confTopo.py
+++ nested-labvm/atd-docker/confTopo/src/confTopo.py
@@ -3,11 +3,20 @@
 import tornado.ioloop
 import tornado.web
 import time
+import yaml
 from datetime import datetime, timedelta
 from os.path import exists
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+ACCESS = '/etc/atd/ACCESS_INFO.yaml'
+gitTempPath = '/opt/atd/'
 APP_PORT = 50010
+sleep_delay = 30
 
+# =================================================
+# Handler Configuration
+# =================================================    
 class ConfigureHandler(tornado.web.RequestHandler):
     def get(self):
         # Check for passed arguments
@@ -15,6 +24,9 @@ class ConfigureHandler(tornado.web.RequestHandler):
             pS("GET STATUS")
 
 
+# =================================================
+# Utility Functions
+# =================================================
 
 def pS(mtype):
     """
@@ -23,6 +35,24 @@ def pS(mtype):
     cur_dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     mmes = "\t" + mtype
     print("[{0}] {1}".format(cur_dt, mmes.expandtabs(7 - len(cur_dt))))
+    
+# =================================================
+# SETUP of environment
+# =================================================
+
+while True:
+    if exists(ACCESS):
+        break
+    else:
+        pS("ERROR", "ACCESS_INFO file is not available...Waiting for {0} seconds".format(sleep_delay))
+        time.sleep(sleep_delay)
+try:
+    f = open(ACCESS)
+    accessinfo = yaml.safe_load(f)
+    f.close()
+    topology = accessinfo['topology']
+except:
+    topology = 'none'
 
 
 if __name__ == "__main__":
@@ -30,6 +60,7 @@ if __name__ == "__main__":
         (r'/api/conftopo', ConfigureHandler)
     ])
     app.listen(APP_PORT)
+
     pS('*** Server Started on {} ***'.format(APP_PORT))
     try:
         tornado.ioloop.IOLoop.instance().start()
