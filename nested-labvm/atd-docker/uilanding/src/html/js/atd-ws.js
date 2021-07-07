5d137ca1086fab7e7e6d4fa672d800aaa36cb75c
Rob Martin <39608748+networkRob@users.noreply.github.com>
2021-07-07 15:01:49 -0500

    added countdown timer to uilanding (#390) (#392)

--- nested-labvm/atd-docker/uilanding/src/html/js/atd-ws.js
+++ nested-labvm/atd-docker/uilanding/src/html/js/atd-ws.js
@@ -8,6 +8,8 @@ else {
 
 atdURL += "/td-ws";
 var ws = new WebSocket(atdURL);
+var event_timer_ids = {};
+var topo_notify = false;
 
 createWS(atdURL);
 
@@ -48,7 +50,10 @@ function createWS(SOCK_URL) {
         }
         else if (received_msg['type'] == 'status') {
             var reg_data = received_msg['data'];
-            console.log(reg_data);
+            if ('uptime' in reg_data) {
+                uptime_data = reg_data['uptime'];
+                instanceCountdown('countdown_timer', uptime_data['boottime'], uptime_data['runtime']);
+            }
             if ('cvp' in reg_data) {
                 _cvp_info = "<h3>CVP " + reg_data['cvp']['version'] + " is currently " + reg_data['cvp']['status'] + "</h3>";
                 if ('tasks' in reg_data) {
@@ -77,4 +82,39 @@ function createWS(SOCK_URL) {
             }));
         }    
     }
+}
+
+function instanceCountdown(element, boot_time, runtime) {
+    var el = document.getElementById(element);
+    var countdown_string = '';
+    var count_style = 'black';
+    if ( event_timer_ids.hasOwnProperty(element) ) {
+        clearInterval(event_timer_ids[element]);
+        delete event_timer_ids[element];
+    }
+    var interval = setInterval(function() {
+        const countdown_diff = (boot_time + ( runtime * 60 * 60 )) - Math.floor( new Date().getTime() / 1000 );
+        if ( countdown_diff > 0 ) {
+            const countdown_parts = {
+                hours: Math.floor((countdown_diff / (60 * 60)) % 24),
+                minutes: Math.floor((countdown_diff / 60) % 60),
+                seconds: Math.floor((countdown_diff) % 60)
+            }
+            if (countdown_diff < (30 * 60) ) {
+                count_style = 'red';
+                // check to see if user has been notified
+                if ( !topo_notify ) {
+                    alert("Your topology will shutdown in " + countdown_parts['minutes'] + " minutes.");
+                    topo_notify = true;
+                }
+            }
+            countdown_string = countdown_parts['hours'].toString().padStart(2,0) + ':' + countdown_parts['minutes'].toString().padStart(2,0) + ':' + countdown_parts['seconds'].toString().padStart(2,0);
+        }
+        else {
+            countdown_string = '00:00:00';
+        }
+        el.innerHTML = countdown_string;
+        el.style.color = count_style;
+    }, 1000);
+    event_timer_ids[element] = interval;
 }
