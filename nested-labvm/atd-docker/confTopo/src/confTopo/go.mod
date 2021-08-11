6f32372143af42a1fc7757ae6fe5efdbb7e07896
Rob Martin <robmartin@arista.com>
2021-08-11 20:36:04 +0000

    udpated devcontainer for GO and started confTopo in go

--- nested-labvm/atd-docker/confTopo/src/confTopo/go.mod
+++ nested-labvm/atd-docker/confTopo/src/confTopo/go.mod
@@ -1,3 +1,9 @@
 module confTopo
 
 go 1.16
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
--- nested-labvm/atd-docker/confTopo/src/confTopo/main.go
+++ nested-labvm/atd-docker/confTopo/src/confTopo/main.go
@@ -1,17 +1,111 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
+	"gopkg.in/yaml.v3"
 )
 
-var upgrader = websocket.Upgrader {
-	ReadBufferSize: 1024,
+type Cred_struct struct {
+	Pw   string `yaml:"pw"`
+	User string `yaml:"user"`
+}
+
+type Login_struct struct {
+	Jump_host Cred_struct `yaml:"jump_host"`
+}
+
+type Access_yaml struct {
+	Login_info     Login_struct `yaml:"login_info"`
+	Nodes          interface{}  `yaml:"nodes"`
+	Topology       string       `yaml:"topology"`
+	Eos_type       string       `yaml:"eos_type"`
+	Version        string       `yaml:"version"`
+	Cvp_mode       string       `yaml:"cvp_mode"`
+	Zone           string       `yaml:"zone"`
+	Name           string       `yaml:"name"`
+	Project        string       `yaml:"project"`
+	Title          string       `yaml:"title"`
+	Cvp            string       `yaml:"cvp"`
+	Disabled_links []string     `yaml:"disabled_links"`
+	Labguides      string       `yaml:"labguides"`
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
+
+var ACCESS string = "/etc/atd/ACCESS_INFO.yaml"
+var SLEEP_DELAY = (60 * time.Second)
+var CVP_NODES [1]string
+var CVP_IP string = "192.168.0.5"
+var TOPO_USER string = ""
+var TOPO_PWD string = ""
+var TEST string = "string"
+
+// ====================================================
+// Utility Functions
+// ====================================================
+
+func decodeID(tmp_data string) (map[string]interface{}, error) {
+	decoded, err := base64.StdEncoding.DecodeString(tmp_data)
+	if err != nil {
+		log.Println("Decode Error: ", err)
+		return nil, err
+	}
+	decoded_json := map[string]interface{}{}
+	if err := json.Unmarshal(decoded, &decoded_json); err != nil {
+		log.Println("Error converting to JSON: ", err)
+		return nil, err
+	}
+
+	return decoded_json, nil
+}
+
+func decodeID_to_string(tmp_data string) (string, error) {
+	decoded, err := base64.StdEncoding.DecodeString(tmp_data)
+	if err != nil {
+		log.Println("Decode Error: ", err)
+		return "", err
+	}
+	var decoded_json string
+	if err := json.Unmarshal(decoded, &decoded_json); err != nil {
+		log.Println("Error converting to JSON: ", err)
+		return "", err
+	}
+
+	return decoded_json, nil
+}
+
+func encodeID(tmp_data map[string]interface{}) string {
+	en_json, err := json.Marshal(tmp_data)
+	if err != nil {
+		log.Fatal("ERROR")
+	}
+	encoded_string := base64.StdEncoding.EncodeToString(en_json)
+	return encoded_string
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+// =============================================================
+// Websocket and Web Handler GO Routines
+// =============================================================
 
 func ReadWS(ws *websocket.Conn, r *http.Request) {
 	defer ws.Close()
@@ -33,26 +127,62 @@ func ReadWS(ws *websocket.Conn, r *http.Request) {
 	log.Printf("Closing WS connection to\n")
 }
 
+// =============================================================
+// Websocket and Web Handlers
+// =============================================================
+
 func TopoRequestHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("GET STATUS\n")
+	params := r.URL.Query()
+	_, ok := params["action"]
+	if ok {
+		encoded_action := params["action"][0]
+		decoded_action, err := decodeID_to_string(encoded_action)
+		if err != nil {
+			log.Printf("Error Decoding id of: %s", encoded_action)
+		}
+		log.Printf("GET: %s", decoded_action)
+		if decoded_action == "cvp_status" {
+			log.Println("Get CVP Status")
+		}
+	} else {
+		log.Println("No action parameter provided")
+	}
 }
 
 func TopoDataHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("New WS Connection")
 	// Check origin for request
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
-	
+
 	// Upgrade the connection to a WebSocket
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
 		return
-	} 
+	}
 	go ReadWS(ws, r)
 
 }
 
 func main() {
+	// Loop and wait until the ACCESS_INFO file exists
+	for {
+		if fileExists(ACCESS) {
+			break
+		} else {
+			log.Printf("ERROR: ACCESS_INFO file is not available...Waiting for %s\n", SLEEP_DELAY)
+		}
+	}
+	// Try to open ACCESS_INFO yaml file
+	accessinfo := Access_yaml{}
+	yaml_file, _ := ioutil.ReadFile(ACCESS)
+	err := yaml.Unmarshal(yaml_file, &accessinfo)
+	if err != nil {
+		log.Printf("%s\n", err)
+	}
+	TOPO_USER = accessinfo.Login_info.Jump_host.User
+	TOPO_PWD = accessinfo.Login_info.Jump_host.Pw
+
 	r := mux.NewRouter()
 	// Routes consist of a path and a handler function.
 	r.HandleFunc("/td-go/conftopo", TopoRequestHandler)
