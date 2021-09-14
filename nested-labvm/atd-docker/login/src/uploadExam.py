2cc2f49c14a21dfa3d496d7a80dd1d7a38b06325
rparkinarista <44816118+rparkinarista@users.noreply.github.com>
2021-09-14 19:13:31 +0100

    Update exam scripts, add save script in login container (#482)
    Co-authored-by: richardap1 <richard.parkin@gmail.com>

--- nested-labvm/atd-docker/login/src/uploadExam.py
+++ nested-labvm/atd-docker/login/src/uploadExam.py
@@ -107,6 +107,7 @@ def getConfigletApplied(configletName,cookies):
     return outputConfiglets
 
 def grabCVPInfo(labPassword,folder):
+    print("Getting CVP details, this may take a few seconds")
     cookies = cvpAuth(labPassword)
     outputConfiglets = getConfiglets(cookies,folder)
     for item in outputConfiglets['data']:
@@ -115,11 +116,12 @@ def grabCVPInfo(labPassword,folder):
             completePath = os.path.join(folder, item['name'])
             with open(completePath +".configlet", 'w') as f:
                 f.write(str(json.dump(appliedConfiglets, f, indent=4)))
+    print("CVP details saved")
 
 def getUserInfo():
     accept = "n"
     emailCounter = 0
-    print("This script requires eAPI to be enabled and functioning. By default this should work, however if any of the settings have been modified it many not work correctly")
+    print("This script requires eAPI to be enabled and functioning. By default this should work, however if any of the settings have been modified it may not work correctly")
     print("You should only upload once you have finished your exam. Once uploaded you will NOT be able to do it again without contacting Arista training or SDN-Pros.")
     while accept != "y":
         accept = input ("Do you accept this? (y/n) ")
@@ -145,6 +147,106 @@ def createUserFile(fullName,email,candidateID,folder,labName,labTopology):
     print("Written user details to file")
 
 
+def grabSwitchDetails(allHostsName,allHostsIP,folder,labPassword):
+    pingDone = 0
+    evpnNOTdone = 0
+    for name, ip in zip(reversed(allHostsName),reversed(allHostsIP)):
+        switch = jsonrpclib.Server("https://arista:{password}@{ipaddress}/command-api".format(password = labPassword, ipaddress = ip))
+        try:
+            config = switch.runCmds(1,["enable", "show running-config"],"text")
+            runConfig = (config[1]["output"])
+            mlag = switch.runCmds(1,["enable", "show mlag","show mlag config-sanity", "show mlag interfaces detail"],"text")
+            mlagOutput = "show mlag\n"
+            mlagOutput += (mlag[1]["output"])
+            mlagOutput += "\nshow mlag config-sanity\n"
+            mlagOutput += (mlag[2]["output"])
+            mlagOutput += "\nshow mlag interfaces detail\n"
+            mlagOutput += (mlag[3]["output"])
+            vxlan = switch.runCmds(1,["enable", "show vxlan address-table", "show mac address-table", "show vxlan flood vtep", "show vxlan vtep detail", "show vxlan vni summary", "show vxlan vni", "show vxlan config-sanity"],"text")
+            vxlanOutput = "show vxlan address-table\n"
+            vxlanOutput += (vxlan[1]["output"])
+            vxlanOutput += "\nshow mac address-table\n"
+            vxlanOutput += (vxlan[2]["output"])
+            vxlanOutput += "\nshow vxlan flood vtep\n"
+            vxlanOutput += (vxlan[3]["output"])
+            vxlanOutput += "\nshow vxlan vtep detail\n"
+            vxlanOutput += (vxlan[4]["output"])
+            vxlanOutput += "\nshow vxlan vni summary\n"
+            vxlanOutput += (vxlan[5]["output"])
+            vxlanOutput += "\nshow vxlan vni\n"
+            vxlanOutput += (vxlan[6]["output"])
+            vxlanOutput += "\nshow vxlan config-sanity\n"
+            vxlanOutput += (vxlan[7]["output"])
+            route = switch.runCmds(1,["enable", "show ip route"],"text")
+            routeOutput = (route[1]["output"])
+            bgpSum = switch.runCmds(1,["enable", "show ip bgp summary"],"text")
+            bgpSumOutput = (bgpSum[1]["output"])
+            if not "host" in name:
+                evpnSum = switch.runCmds(1,["enable", "show bgp evpn summary"],"text")
+                evpnSumOutput = (evpnSum[1]["output"])
+                evpnBGP = switch.runCmds(1,["enable", "show bgp evpn"],"text")
+                evpnBGPOutput = (evpnBGP[1]["output"])
+                evpnNOTdone = 1
+            intSum = switch.runCmds(1,["enable", "show ip interface brief"],"text")
+            intSumOutput = (intSum[1]["output"])
+            vlanSum = switch.runCmds(1,["enable", "show vlan"],"text")
+            vlanSumOutput = (vlanSum[1]["output"])
+            if "host1" in name:
+                pingSum = switch.runCmds(1,["enable", "ping 172.16.200.10"],"text")
+                pingSumOutput = (pingSum[1]["output"])
+                pingDone = 1
+        except Exception as e:
+            print(str(e))
+            print("Check eAPI is enabled on {switch}".format(switch = name))
+        else:
+            filename = str(name) + "-running" + ".txt"
+            completePath = os.path.join(folder, filename)
+            with open(completePath, 'w') as f:
+                f.write(runConfig)
+            filename = str(name) + "-MLAG" + ".txt"
+            completePath = os.path.join(folder, filename)
+            with open(completePath, 'w') as f:
+                f.write(mlagOutput)
+            filename = str(name) + "-Route" + ".txt"
+            completePath = os.path.join(folder, filename)
+            with open(completePath, 'w') as f:
+                f.write(routeOutput)
+            filename = str(name) + "-BGPsummary" + ".txt"
+            completePath = os.path.join(folder, filename)
+            with open(completePath, 'w') as f:
+                f.write(bgpSumOutput)
+            if evpnNOTdone == 1:
+                filename = str(name) + "-EVPNsummary" + ".txt"
+                completePath = os.path.join(folder, filename)
+                with open(completePath, 'w') as f:
+                    f.write(evpnSumOutput)
+                filename = str(name) + "-EVPN" + ".txt"
+                completePath = os.path.join(folder, filename)
+                with open(completePath, 'w') as f:
+                    f.write(evpnBGPOutput)
+                evpnNOTdone = 0
+            filename = str(name) + "-INTsummary" + ".txt"
+            completePath = os.path.join(folder, filename)
+            with open(completePath, 'w') as f:
+                f.write(intSumOutput)
+            filename = str(name) + "-VXLAN" + ".txt"
+            completePath = os.path.join(folder, filename)
+            with open(completePath, 'w') as f:
+                f.write(vxlanOutput)
+            filename = str(name) + "-VLANsummary" + ".txt"
+            completePath = os.path.join(folder, filename)
+            with open(completePath, 'w') as f:
+                f.write(vlanSumOutput)
+            if pingDone == 1:
+                filename = str(name) + "-PINGsummary" + ".txt"
+                completePath = os.path.join(folder, filename)
+                with open(completePath, 'w') as f:
+                    f.write(pingSumOutput)
+                pingDone = 0
+            print("Switch {switch} config and output saved".format(switch = name))
+
+
+
 
 def main():
     labPassword, labTopology, labName = readLabDetails()
@@ -157,25 +259,14 @@ def main():
             os.makedirs(folder)
         except OSError as exc: # Guard against race condition
             raise
-    for name, ip in zip(allHostsName,allHostsIP):
-        switch = jsonrpclib.Server("https://arista:{password}@{ipaddress}/command-api".format(password = labPassword, ipaddress = ip))
-        try:
-            config = switch.runCmds(1,["enable", "show running-config"],"text")
-            runConfig = (config[1]["output"])
-        except:
-            print("Check eAPI is enabled on {switch}".format(switch = name))
-        else:
-            filename = str(name) + ".txt"
-            completePath = os.path.join(folder, filename)
-            with open(completePath, 'w') as f:
-                f.write(runConfig)
-            print("Switch {switch} config saved".format(switch = name))
+    grabSwitchDetails(allHostsName,allHostsIP,folder,labPassword)
     tarFile = folder + "-" + candidateID
     grabCVPInfo(labPassword,folder)
     createUserFile(fullName,email,candidateID,folder,labName,labTopology)
     print("This file will be created and uploaded " + tarFile)
     with tarfile.open(tarFile, "w:gz") as tar:
         tar.add(os.getcwd() + "/" + folder, arcname=os.path.basename(tarFile))
+        tar.add("apps/coder/",arcname=os.path.basename(tarFile))
     ftpUpload(tarFile)
 
 
