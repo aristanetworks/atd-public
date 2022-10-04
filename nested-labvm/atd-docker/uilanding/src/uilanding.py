009d6d3b03351451c498b0a41a9862c8c3ce3f46
Rob Martin <robmartin@arista.com>
2022-10-04 18:36:55 +0000

    added schema check in uilanding

--- nested-labvm/atd-docker/uilanding/src/uilanding.py
+++ nested-labvm/atd-docker/uilanding/src/uilanding.py
@@ -49,6 +49,10 @@ FUNC_STATE = 'https://us-central1-{0}.cloudfunctions.net/atd-state'.format(PROJE
 NAME = host_yaml['name']
 ZONE = host_yaml['zone']
 TOPO = host_yaml['topology']
+if 'schema' in host_yaml:
+    SCHEMA = host_yaml['schema']
+else:
+    SCHEMA = 1
 
 # Add a check for the title parameter for legacy deployment catches
 if 'title' in host_yaml:
@@ -248,7 +252,8 @@ def getUptime(instanceIP):
         else:
             instance_data['runtime'] = 8
         return(instance_data)
-    except:
+    except Exception as e:
+        traceback.print_exc()
         return({
             'boottime': 0,
             'uptime': 0,
@@ -261,7 +266,10 @@ def getEventStatus(instanceName, instanceZone):
     Function to get the currnet status of an instance.
     """
     try:
-        response = requests.get(FUNC_STATE + "?function=state&instance={0}&zone={1}".format(instanceName, instanceZone))
+        if SCHEMA == 2:
+            response = requests.get(FUNC_STATE + "?function=state&instance={0}-eos&zone={1}".format(instanceName, instanceZone))
+        else:
+            response = requests.get(FUNC_STATE + "?function=state&instance={0}&zone={1}".format(instanceName, instanceZone))
         return(response.json())
     except ValueError:
         pS("Value Error retrieving status for {0}".format(instanceName))
