5e5df041f3ebb88f3d63193a86db014b621490ad
Rob Martin <39608748+networkRob@users.noreply.github.com>
2023-06-29 13:22:01 -0500

    rm rlab ceos (#742) (#743)
    * rewrite for ceosbuilder
    
    * updated ceosbuilder
    
    * topo name update
    
    * removing ceosbuilder from compose
    
    * updated ceos name in builder
    
    * adding mtu adjust to ceosbuilder
    
    * updating ceosbuild
    
    * updates to ceosbuilder dev name
    
    * adding new docker build run script to be used during image builds
    
    * updated routing build file
    
    * updated ceos build script

---
+++ nested-labvm/atd-docker/docker_build_run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Starting one-time build containers"
+
+docker run --name atd-ceosbuilder -d --rm -v /etc/atd:/etc/atd:ro -v /opt/atd:/opt/atd:ro -v /opt/ceos:/opt/ceos:rw -e PYTHONUNBUFFERED=1 us.gcr.io/beta-atds/atddocker_ceosbuilder:3.1.1
