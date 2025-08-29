3a724ee441fef80d70ec00dea9b6d10681fddcf6
networkRob-Arista <robmartin@arista.com>
2025-08-29 12:24:13 -0500

    Merge pull request #945 from aristanetworks/snapshot-restore
    Forcing the recreation of the vtep startup script

--- nested-labvm/services/cvpStartup/cvpStartup.sh
+++ nested-labvm/services/cvpStartup/cvpStartup.sh
@@ -24,16 +24,13 @@ cd /opt/atd/nested-labvm/atd-docker
 # if VTEP file present
 if [ -f "/etc/atd/.provisioned" ]
 then
-    if [ -f "/etc/atd/.init" ]
-    then
-        bash docker_run.sh
-        while : ; do
-            [[ -f "/etc/atd/.vtep.sh" ]] && break
-            echo "Pausing until file exists."
-            sleep 1
-        done
-        bash /etc/atd/.vtep.sh
-    else
-        bash /etc/atd/.vtep.sh
-    fi
+    # Forcing the recreation of the vtep startup script for snapshot restores
+    rm /etc/atd/.vtep.sh
+    bash docker_run.sh
+    while : ; do
+        [[ -f "/etc/atd/.vtep.sh" ]] && break
+        echo "Pausing until file exists."
+        sleep 1
+    done
+    bash /etc/atd/.vtep.sh
 fi
